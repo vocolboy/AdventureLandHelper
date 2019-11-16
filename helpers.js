1c8d2443e6789ba4f0ad4168075a3c697d8f2888
vocolboy <vocolboy@gmail.com>
2019-11-16 17:12:18 +0800

    add `item_sort`,`get_party_list` function

--- helpers.js
+++ helpers.js
@@ -3,12 +3,82 @@
  *  取得附近玩家
  *  @returns {Array} players data
  */
-function getNearestPlayers(){
+function get_nearest_players() {
     let players = [];
 
-    Object.keys(parent.entities).forEach(function(key) {
-        if(parent.entities[key].player){ players.push(parent.entities[key])};
+    Object.keys(parent.entities).forEach(function (key) {
+        if (parent.entities[key].player) {
+            players.push(parent.entities[key])
+        }
     });
 
     return players;
+}
+
+/**
+ * Get party list
+ * 取得隊員名稱
+ * @return {Array} Only players name
+ */
+function get_party_list() {
+    return parent.party_list;
+}
+
+/**
+ * Sort item by name
+ * 依造物品名稱排序
+ * @returns {void}
+ */
+function item_sort() {
+    let items = character.items;
+    items.forEach(function (element, index) {
+        if (!element) {
+            items[index] = {
+                "name": "zzz"
+            }
+        }
+    });
+
+    //bubble sort by name
+    let backpack_size = items.length - 1;
+    for (let j = 0; j < backpack_size; j++) {
+        let done = true;
+        for (let i = 0; i < backpack_size - j; i++) {
+            if (items[i].name > items[i + 1].name) {
+                if (!items[i].a && items[i].name !== 'zzz') {
+                    items[i].a = i
+                }
+                if (!items[i + 1].a && items[i + 1].name !== 'zzz') {
+                    items[i + 1].a = i + 1
+                }
+
+                let temp = items[i];
+                items[i] = items[i + 1];
+                items[i + 1] = temp;
+                done = false;
+            }
+        }
+        if (done) {
+            break;
+        }
+    }
+
+    //item move
+    items.forEach(function (item, index) {
+        if (!item.a) return;
+
+        parent.socket.emit("imove", {
+            a: item.a,
+            b: index
+        });
+
+        moved_item = items.find(function (moved_item) {
+            return moved_item.a === index;
+        });
+
+        if(!moved_item) return;
+
+        moved_item.a = item.a;
+        item.a = 0;
+    });
 }
